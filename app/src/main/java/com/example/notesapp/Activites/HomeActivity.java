90597da9516492e28a96b6f9746884675f9a0c4d
minnatallah97 <minnatallah.279@gmail.com>
2019-12-29 04:12:07 +0300

    Edit By Minna

--- app/src/main/java/com/example/notesapp/Activites/HomeActivity.java
+++ app/src/main/java/com/example/notesapp/Activites/HomeActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.notesapp.Activites;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/notesapp/Activites/Main2Activity.java
+++ app/src/main/java/com/example/notesapp/Activites/Main2Activity.java
@@ -4,21 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.notesapp.R;
 
 public class Main2Activity extends AppCompatActivity {
+    TextView ti,de,ca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ti=findViewById(R.id.ti_tv);
+        de=findViewById(R.id.de_tv);
+        ca=findViewById(R.id.ca_tv);
         String title = getIntent().getStringExtra("title");
         String Desc = getIntent().getStringExtra("Desc");
         String Category = getIntent().getStringExtra("Category");
         Log.i("our value ",title);
         Log.i("our value ",Desc);
+        ti.setText(title);
+        de.setText(Desc);
+        ca.setText(Category);
         Toast.makeText(this, ""+title+Desc, Toast.LENGTH_SHORT).show();
     }
 }
